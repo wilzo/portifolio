69cd8e4f9e33b68e216a93557e8c95022a9a5f7f
Javier Diaz <coderdiaz@gmail.com>
2025-04-17 23:28:33 -0600

    feat: enabled support for mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), mdx({
+    optimize: true,
+  })],
 });
