0ba9a44ddecd2c29f4e50cf2ceff4981fe1fb55a
Javier Diaz <coderdiaz@gmail.com>
2025-04-18 01:54:00 -0600

    feat: enabling shiki and read time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,21 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import { remarkReadingTime } from './src/lib/remark.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    shikiConfig: {
+      theme: 'gruvbox-dark-medium',
+    }
+  },
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [react(), mdx({
     optimize: true,
+    syntaxHighlight: 'shiki',
   })],
 });
