4a6a978de46a1e5d92acb5f1b006817c4101e4bc
Javier Diaz <coderdiaz@gmail.com>
2025-06-25 19:45:10 -0600

    style: formatting code

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,12 +33,26 @@ export default defineConfig({
         cssVariable: '--font-inter',
         provider: 'local',
         variants: [
-          { src: ['./src/assets/fonts/Inter-Regular.woff2'], weight: 400 },
-          { src: ['./src/assets/fonts/Inter-Medium.woff2'], weight: 500 },
-          { src: ['./src/assets/fonts/Inter-SemiBold.woff2'], weight: 600 },
-          { src: ['./src/assets/fonts/Inter-Bold.woff2'], weight: 700 },
-          { src: ['./src/assets/fonts/Inter-ExtraBold.woff2'], weight: 800 },
-          { src: ['./src/assets/fonts/InterVariable.woff2'] },
+          {
+            src: ['./src/assets/fonts/Inter-Regular.woff2'],
+            weight: 400,
+          },
+          {
+            src: ['./src/assets/fonts/Inter-Medium.woff2'],
+            weight: 500,
+          },
+          {
+            src: ['./src/assets/fonts/Inter-SemiBold.woff2'],
+            weight: 600,
+          },
+          {
+            src: ['./src/assets/fonts/Inter-Bold.woff2'],
+            weight: 700,
+          },
+          {
+            src: ['./src/assets/fonts/Inter-ExtraBold.woff2'],
+            weight: 800,
+          },
         ],
       },
       {
