d2c958c1d57cc61cb4a897b8b30e200d7d4d2f3b
Junior Wilson <seuemail@example.com>
2025-07-14 11:20:38 -0300

    first commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 import type { z } from 'astro/zod';
-import MetaDefaultImage from '@/assets/images/meta-default.jpg';
+import MetaDefaultImage from '@/assets/images/album.png';
 import avatar from '@/assets/images/avatar.jpeg';
 import type { seoSchemaWithoutImage } from '@/content.config';
 import astroConfig from 'astro.config.mjs';
@@ -11,36 +11,36 @@ export type AuthorInfo = {
   username?: string;
   location?: string;
   pronouns?: string;
-}
+};
 
 export type Seo = z.infer<typeof seoSchemaWithoutImage> & {
   image?: any;
-}
+};
 
 type DefaultConfigurationType = {
-  baseUrl: string,
+  baseUrl: string;
   author: AuthorInfo;
   seo: Seo;
-}
+};
 
 export const DEFAULT_CONFIGURATION: DefaultConfigurationType = {
   baseUrl: astroConfig.site || 'https://getcvfolio.com',
   author: {
     avatar,
-    name: 'Javier Diaz Chamorro',
-    headline: 'Engineering and Design',
-    username: 'coderdiaz',
-    location: 'Mexico City',
-    pronouns: 'He/Him',
+    name: 'Wilson Hernandes Cardoso Junior',
+    headline: 'Desenvolvedor FullStack',
+    username: 'Wilson',
+    location: 'Cuiabá-MT',
   },
   seo: {
-    title: 'CV Folio — An Astro template inspired on Read.cv',
-    description: 'Clean and aesthetic portfolio website for developers and designers',
+    title: 'Portifolio',
+    description:
+      'Clean and aesthetic portfolio website for developers and designers',
     type: 'website',
     image: MetaDefaultImage,
     twitter: {
-      creator: '@cvfolio'
+      creator: '@cvfolio',
     },
     robots: 'noindex, nofollow',
-  }
+  },
 };
